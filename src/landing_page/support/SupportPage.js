77725353aee78b33cee6609ff97bb03e7093d0b5
mahak540 <162689029+mahak540@users.noreply.github.com>
2026-04-16 15:58:44 +0530

    Update SupportPage.js

--- src/landing_page/support/SupportPage.js
+++ src/landing_page/support/SupportPage.js
@@ -3,8 +3,7 @@ import React from "react";
 import Hero from "./Hero";
 import CreateTicket from "./CreateTicket";
 
-import Navbar from "../Navbar";
-import Footer from "../Footer";
+
 
 function PricingPage() {
   return (
