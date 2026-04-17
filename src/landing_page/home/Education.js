5f30929c02330f0249e3823fe48780dbd46970a4
mahak540 <162689029+mahak540@users.noreply.github.com>
2026-04-17 13:21:46 +0530

    Update Education.js

--- src/landing_page/home/Education.js
+++ src/landing_page/home/Education.js
@@ -1,11 +1,11 @@
 import React from "react";
-
+import {Link} from "react-router-dom";
 function Education() {
   return (
     <div className="container mt-5">
       <div className="row">
         <div className="col-6">
-          <img src="edu.svg" style={{ width: "70%" }} />
+          <img src="edu.svg" alt="" style={{ width: "70%" }} />
         </div>
         <div className="col-6">
           <h1 className="mb-3 fs-2">Free and open market education</h1>
@@ -13,16 +13,16 @@ function Education() {
             Varsity, the largest online stock market education book in the world
             covering everything from the basics to advanced trading.
           </p>
-          <a href="" style={{ textDecoration: "none" }}>
+          <Link to="" style={{ textDecoration: "none" }}>
             Versity <i className="fa fa-long-arrow-right" aria-hidden="true"></i>
-          </a>
+          </Link>
           <p className="mt-5">
             TradingQ&A, the most active trading and investment community in
             India for all your market related queries.
           </p>
-          <a href="" style={{ textDecoration: "none" }}>
+          <Link to="" style={{ textDecoration: "none" }}>
             TradingQ&A <i className="fa fa-long-arrow-right" aria-hidden="true"></i>
-          </a>
+          </Link>
         </div>
       </div>
     </div>
