c2b64546e98522d726d299347cbdd5b266cf0f85
mahak540 <162689029+mahak540@users.noreply.github.com>
2026-04-17 11:05:41 +0530

    Update Brokerage.js

--- src/landing_page/pricing/Brokerage.js
+++ src/landing_page/pricing/Brokerage.js
@@ -1,13 +1,13 @@
 import React from "react";
-
+import {Link} from "react-router-dom";
 function Brokerage() {
   return (
     <div className="container">
       <div className="row p-5 mt-5 text-center border-top">
         <div className="col-8 p-4">
-          <a href="" style={{ textDecoration: "none" }}>
+          <Link href="" style={{ textDecoration: "none" }}>
             <h3 className="fs-5">Brokerage calculator</h3>
-          </a>
+          </Link>
           <ul
             style={{ textAlign: "left", lineHeight: "2.5", fontSize: "12px" }}
             className="text-mut"
@@ -36,9 +36,9 @@ function Brokerage() {
           </ul>
         </div>
         <div className="col-4 p-4">
-          <a href="" style={{ textDecoration: "none" }}>
+          <Link href="" style={{ textDecoration: "none" }}>
             <h3 className="fs-5">List of charges</h3>
-          </a>
+          </Link>
         </div>
       </div>
     </div>
